ddf6f43e718a1203ec040719b8bc3d00694d5fe4
chengzhipcx <1264025726@qq.com>
2015-04-08 08:38:14 +0800

    第一题   提交人 孙雪媛

---
+++ Test1.java
@@ -0,0 +1,12 @@
+package test;
+/**
+ * µÚÒ»Ìâ helloWord
+ * 
+ * @author ËïÑ©æÂ
+ *
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("hello,world!");
+	}
+}
